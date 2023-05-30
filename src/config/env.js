efc9c50b1d999d11a39c6b6da1eeb5095f76d8d6
Lauty128 <lautarosilverii@gmail.com>
2023-05-30 10:50:33 -0300

    variable PORT added

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 
+export const port = process.env.PORT
+
 export const db_name = process.env.DB_NAME;
 export const db_user = process.env.DB_USER;
 export const db_password = process.env.DB_PASSWORD ;
