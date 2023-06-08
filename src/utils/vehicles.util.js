1c4a04e5775b62dc5fbda93cbd0ac04623c0d25b
Lauty128 <lautarosilverii@gmail.com>
2023-06-08 08:49:39 -0300

    import error fixed

--- src/utils/vehicles.util.js
+++ src/utils/vehicles.util.js
@@ -1,12 +1,11 @@
 //---- Services
-import Service from "../services/api.service.js";
+import vehiclesService from "../services/vehicles.service.js"
 
 export async function readData(callback, { size = 20 } = {}){
-    const service = new Service('vehicle')
     const attributes = ['id_vehicle','version','image','extra_images','price','model','type','traction','km']
 
     let pagination = { offset:0 , limit: size }
-    const vehiclesData = await service.findAll({ pagination, attributes })
+    const vehiclesData = await vehiclesService.findAll({ pagination, attributes })
     let data = vehiclesData.data
 
     const total_of_vehicles = vehiclesData.total
@@ -14,7 +13,7 @@ export async function readData(callback, { size = 20 } = {}){
         if(page > 0){
             //---- If there are more of one page then read the other page in the data base
             pagination.offset = (page * size) 
-            const newVehiclesData = await service.findAll({ pagination, attributes })
+            const newVehiclesData = await vehiclesService.findAll({ pagination, attributes })
             data = newVehiclesData.data
         }
 
